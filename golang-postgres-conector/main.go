e0de90e74d99137a4cbb79ca78b0b08d8cf9a4fe
Jonas Góes <jonasdsg1@gmail.com>
2022-06-02 09:56:17 -0300

    Connecting with database after created the database docker container, the connection was tested

--- golang-postgres-conector/main.go
+++ golang-postgres-conector/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
 
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	//Data source information.
+	ds := "user=postgres dbname=postgres password=postgres sslmode=disable"
+	//openning a database conection
+	conn, err := sql.Open("postgres", ds)
+	if err == nil {
+		fmt.Println(conn.Stats())
+	}
+
+	defer conn.Close()
 }
