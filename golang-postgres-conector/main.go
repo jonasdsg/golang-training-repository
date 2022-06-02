93548a6c9320795f504543440282b6ae3a93f2d6
Jonas Góes <jonasdsg1@gmail.com>
2022-06-02 11:46:36 -0300

    Querying and listing people

--- golang-postgres-conector/main.go
+++ golang-postgres-conector/main.go
@@ -12,9 +12,33 @@ func main() {
 	ds := "user=postgres dbname=postgres password=postgres sslmode=disable"
 	//openning a database conection
 	conn, err := sql.Open("postgres", ds)
-	if err == nil {
-		fmt.Println(conn.Stats())
+
+	//Throwing erro
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query("select * from person")
+
+	if err != nil {
+		panic(err)
 	}
 
-	defer conn.Close()
+	people := []Person{}
+	for rows.Next() {
+		person := Person{}
+		rows.Scan(&person.Id, &person.First_name, &person.Middle_name, &person.Last_name, &person.Contacts)
+		people = append(people, person)
+	}
+
+	fmt.Println(people)
+}
+
+type Person struct {
+	Id          int
+	First_name  string
+	Middle_name string
+	Last_name   string
+	Contacts    string
 }
