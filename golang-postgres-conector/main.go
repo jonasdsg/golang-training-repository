e57773dee12f14072db016a4d98dfebf730f227a
Jonas Góes <jonasdsg1@gmail.com>
2022-06-04 20:12:09 -0300

    SOLID change - division of responsibilities. The main method was making too many things, so i had to split it

--- golang-postgres-conector/main.go
+++ golang-postgres-conector/main.go
@@ -4,28 +4,51 @@ import (
 	"database/sql"
 	"fmt"
 	"golang-training-repository/golang-postgres-conector/model"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
+var Conn *sql.DB
+
 func main() {
-	people := []model.Person{}
+	Conn = Connect()
+	Insert(model.Person{
+		Id:          0,
+		First_name:  "Cath",
+		Middle_name: "Little",
+		Last_name:   "Pop",
+		Contacts:    "catlittlepop@candies.co",
+	})
+	fmt.Println(FindAll())
+}
+
+func Connect() *sql.DB {
 	//Data source information.
 	ds := "user=postgres dbname=postgres password=postgres sslmode=disable"
+	defer ErrorHandler()
 	//openning a database conection
-	conn, err := sql.Open("postgres", ds)
+	if conn, err := sql.Open("postgres", ds); err != nil {
+		panic(err)
+	} else {
+		return conn
+	}
+}
 
-	//Throwing erro
-	if err != nil {
+func Insert(person model.Person) bool {
+	//Inserting data into person table
+	defer ErrorHandler()
+	if _, err := Conn.Exec("insert into person(first_name, middle_name, last_name, contacts) values ( $1 ,$2 , $3, $4 )", person.First_name, person.Middle_name, person.Last_name, person.Contacts); err != nil {
 		panic(err)
 	}
-	defer conn.Close()
-	//	Insertin someone
-	if _, err := conn.Exec("insert into person(first_name, middle_name, last_name, contacts) values ('Kath','Glass', 'Pool', 'kgpool@git.co')"); err != nil {
-		panic(err)
-	}
+	return true
+}
 
-	if rows, err := conn.Query("select * from person"); err != nil {
+func FindAll() []model.Person {
+	people := []model.Person{}
+
+	defer ErrorHandler()
+	if rows, err := Conn.Query("select * from person"); err != nil {
 		panic(err)
 	} else {
 
@@ -36,6 +59,14 @@ func main() {
 		}
 
 	}
+	return people
+}
 
-	fmt.Println(people)
+func ErrorHandler() {
+	//Handle an errer when it occurs
+	err := recover()
+	if err != nil {
+		log.Fatal("Something went wrong..", err)
+		Conn.Close()
+	}
 }
